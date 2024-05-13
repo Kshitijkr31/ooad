c5fd441d12f6b020211a4ca051737f0ef3a7519d
Kshitij Kumar <143714416+Kshitijkr31@users.noreply.github.com>
2024-05-13 10:48:02 +0530

    Create PaintView.java

---
+++ PaintView.java
@@ -0,0 +1,59 @@
+package com.example.paintbrush;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+
+public class PaintView extends View {
+
+    private Paint paint;
+    private Path path;
+
+    public PaintView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setupPaint();
+    }
+
+    private void setupPaint() {
+        paint = new Paint();
+        paint.setColor(Color.BLACK);
+        paint.setAntiAlias(true);
+        paint.setStrokeWidth(10);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeJoin(Paint.Join.ROUND);
+        paint.setStrokeCap(Paint.Cap.ROUND);
+
+        path = new Path();
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        canvas.drawPath(path, paint);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        float xPos = event.getX();
+        float yPos = event.getY();
+
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                path.moveTo(xPos, yPos);
+                return true;
+            case MotionEvent.ACTION_MOVE:
+                path.lineTo(xPos, yPos);
+                break;
+            default:
+                return false;
+        }
+
+        invalidate();
+        return true;
+    }
+}
